df1a51f7146db1fbb560b74a94237ccdd40622a8
jonathan warner <j.m.warners@gmail.com>
2020-07-09 13:52:42 -0700

    Working on Step 3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/jonivander')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +50,22 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function idCardMaker (person){
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('card-info')
+  const name = document.createElement('h3')
+  const userName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const gitAddress = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  
+}
 
 /*
   List of LS Instructors Github username's:
